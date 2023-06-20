899525beef88857fad7b93a72dba48ec75a6e12d
Himanshu <59042998+Hmnshu08@users.noreply.github.com>
2023-06-20 18:45:26 +0530

    Resiliance4j setting
    Some setup for circuit breaker.

--- guest-service/src/main/java/com/hms/guest/GuestServiceApplication.java
+++ guest-service/src/main/java/com/hms/guest/GuestServiceApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuestServiceApplication {
 
+//	static String name = "Karti123";
+
 	public static void main(String[] args) {
 		SpringApplication.run(GuestServiceApplication.class, args);
+
+//		for (int i = 0; i < name.length(); i++) {
+//			if (Character.isDigit(name.charAt(i)) == true) {
+//				System.out.println("Character : " +name.charAt(i)+" "  + "Number not present");
+//			} else {
+//				System.out.println("Character : " +name.charAt(i)+" "  +  " digit present.");
+//			}
+//			System.out.println(name.charAt(i));
+//
+//		}
+
 	}
 
 }
--- guest-service/src/main/java/com/hms/guest/controller/GuestController.java
+++ guest-service/src/main/java/com/hms/guest/controller/GuestController.java
@@ -3,6 +3,8 @@ package com.hms.guest.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +17,36 @@ import com.hms.guest.entity.Guest;
 import com.hms.guest.exception.GuestNotFoundException;
 import com.hms.guest.repository.GuestRepository;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 public class GuestController {
+	
+	private Logger logger = 
+			LoggerFactory.getLogger(GuestController.class);
+	
 	@Autowired
 	private GuestRepository repo;
 	
+//	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//	@Retry(name = "guest", fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name="default")
+	@Bulkhead(name="guest")
 	@GetMapping("/guest")
 	public List<Guest> getAllGuest() {
+		logger.info("Api call received");
 		return repo.findAll();
+		
 		
 	}
 
+//	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//	@Retry(name = "guest", fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name="default")
+	@Bulkhead(name="guest")
 	@GetMapping("/guest/{id}")
 	public Optional<Guest> getGuestById(@PathVariable Long id) {
 		Optional<Guest> guest = repo.findById(id);
@@ -36,11 +57,13 @@ public class GuestController {
 		return guest;
 	}
 	
+//	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
 	@PostMapping("/guest")
 	public Guest createGuest(@RequestBody Guest guest) {
 		return repo.save(guest);
 	}
 	
+//	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
 	@DeleteMapping("/guest/{id}")
 	public void deleteGuestById(@PathVariable Long id) 
 	{
@@ -51,4 +74,9 @@ public class GuestController {
 		}
 		repo.deleteById(id);
 	}
+	
+	//if service doesn't responds then this will get called
+//	public String hardcodedResponse(Exception ex) {
+//		return "fallback-response";
+//	}
 }
